32b8903225ca2d64404b301440e9e3421f8ad344
Michael Jones <michael.jones90@me.com>
2026-06-04 14:20:29 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-BetterRest/HackingWithSwiftUI_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_BetterRestApp.swift
+//  HackingWithSwiftUI-BetterRest
+//
+//  Created by Michael Jones on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

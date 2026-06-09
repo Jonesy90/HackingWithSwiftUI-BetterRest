e5f6ed5a3aa4dfa76ae83accd46b9eff8ffbcace
Michael Jones <michael.jones90@me.com>
2026-06-09 21:44:54 +0100

    - Additional comments.

--- HackingWithSwiftUI-BetterRest/ContentView.swift
+++ HackingWithSwiftUI-BetterRest/ContentView.swift
@@ -63,27 +63,35 @@ struct ContentView: View {
         }
     }
     
+    /// Uses CoreML model to predict the ideal time to go to bed based on when the user wants to wake up, how much sleep they want and how often they drink coffee.
     private func calculateBedtime() {
         do {
-            let config = MLModelConfiguration() //TODO: Look into this!
-            let model = try SleepCalculator(configuration: config) // This reads all the data and sends back a predicted output.
+            // Sets up the ML environment and creates an instance of the CoreML model (SleepCalculator).
+            let config = MLModelConfiguration() // The configuration settings for create or updating a CoreML model.
+            let model = try SleepCalculator(configuration: config) // Reads all the data and sends back a predicted output.
             
+            // Extracts the hour and minute from the wakeUp @State property.
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
             
+            // Calls to create a prediction using wake, estimatedSleep and coffee as parameters.
+            // Returns a prediction that contains 'actualSleep', which is how much sleep the user actually needs (in seconds).
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
+            // Calculates what time the user should go to bed.
             let sleepTime = wakeUp - prediction.actualSleep
             
             alertTitle = "Your ideal bedtime is..."
             alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
             
         } catch {
+            // If anything goes wrong (e.g., model failing to load, an error message will appear instead.
             alertTitle = "Error"
             alertMessage = "Something went wrong. Please try again."
         }
         
+        // Triggers the Alert view to show one of the results (Prediction or Error message).
         showingAlert = true
     }
 }

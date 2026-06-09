0b890915fb9df2c69c16df339d1138e1ba753848
Michael Jones <michael.jones90@me.com>
2026-06-09 21:31:15 +0100

    - Including CoreML - NavigationStack and Form. - Multiple @State properties to handle data input (e.g., sleepAmount and coffeeAmount). - Static computed property to create a custom Date (07:00). - calculateBedtime function to bring in the CoreML model, reads all the data within the model, and returns a prediction based off the @State property inputs.

--- HackingWithSwiftUI-BetterRest/ContentView.swift
+++ HackingWithSwiftUI-BetterRest/ContentView.swift
@@ -5,17 +5,86 @@
 //  Created by Michael Jones on 04/06/2026.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    //Static Variable. This means it belongs to the 'ContentView' Struct itself, rather than an instance of 'ContentView'.
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep:")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily Coffee Consumption")
+                        .font(.headline)
+                    
+                    //Specialised form of the markdown, which is common in free-text.
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20, step: 1)
+                }
+            }
+            .navigationTitle(Text("Better Rest"))
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK", action: {})
+            } message: {
+                Text(alertMessage)
+            }
         }
-        .padding()
+    }
+    
+    private func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration() //TODO: Look into this!
+            let model = try SleepCalculator(configuration: config) // This reads all the data and sends back a predicted output.
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Something went wrong. Please try again."
+        }
+        
+        showingAlert = true
     }
 }
 
